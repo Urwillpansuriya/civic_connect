bcb74e58c663324412def9177f3ac34046c45aa6
Urwillpansuriya <urwillp@gmail.com>
2026-04-06 15:55:36 +0530

    F

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -113,6 +113,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+const API_URL = "https://civic-connect-hams.onrender.com";
 
 function Login() {
   const [email, setEmail] = useState('');
@@ -128,7 +129,7 @@ function Login() {
     
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', { email, password });
+      const res = await axios.post(`${API_URL}/api/auth/login`, { email, password });
 
       localStorage.setItem('token', res.data.token);
       localStorage.setItem('user', JSON.stringify(res.data.user));
